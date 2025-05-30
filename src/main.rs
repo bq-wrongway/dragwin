121ba0e74f9be9580fb4ed16dd5640d1312a3094
bq-wrongway <sesa.zeljko@gmail.com>
2025-05-30 22:11:32 +0200

    just a boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
+mod utils;
+
 use iced::Length::Fill;
 use iced::mouse::Interaction;
-use iced::widget::container::Style;
-use iced::widget::{button, column, container, mouse_area, row, text};
+use iced::widget::container::{self, Style};
+use iced::widget::{button, mouse_area, row, text};
 use iced::window::drag_resize;
-use iced::{Background, Color, Task, Theme, theme, window};
-use iced::{Element, Length};
+use iced::{Background, Element};
+use iced::{Color, Task, Theme, theme, window};
 use std::fmt::Debug;
-
+use utils::dragwin::view;
 pub fn main() -> iced::Result {
     iced::application(Example::new, Example::update, Example::view)
         .window(window::Settings {
@@ -15,6 +17,7 @@ pub fn main() -> iced::Result {
 
             ..Default::default()
         })
+        .theme(|_| Theme::CatppuccinMocha)
         .style(|_, _| theme::Style {
             background_color: Color::TRANSPARENT,
             text_color: Color::WHITE,
@@ -25,190 +28,35 @@ pub fn main() -> iced::Result {
 struct Example {
     value: u32,
 }
-
 #[derive(Debug, Clone)]
-enum Message {
-    Drag,
-    NorthWest,
-    North,
-    NorthEast,
-    West,
-    East,
-    South,
-    SouthWest,
-    SouthEast,
-    Close,
+pub enum Message {
+    Shakira(utils::dragwin::Message),
 }
-
 impl Example {
     fn new() -> (Self, Task<Message>) {
         (Self { value: 9 }, Task::none())
     }
-
     fn update(&mut self, message: Message) -> Task<Message> {
         match message {
-            Message::Drag => {
-                self.value += 1;
-                window::get_latest().and_then(window::drag)
-                // Task::none()
+            Message::Shakira(m) => {
+                match m {
+                    utils::dragwin::Message::Drag => todo!(),
+                    utils::dragwin::Message::Maximize => todo!(),
+                    utils::dragwin::Message::NorthWest => todo!(),
+                    utils::dragwin::Message::North => todo!(),
+                    utils::dragwin::Message::NorthEast => todo!(),
+                    utils::dragwin::Message::West => todo!(),
+                    utils::dragwin::Message::East => todo!(),
+                    utils::dragwin::Message::South => todo!(),
+                    utils::dragwin::Message::SouthWest => todo!(),
+                    utils::dragwin::Message::SouthEast => todo!(),
+                    utils::dragwin::Message::Close => todo!(),
+                }
+                Task::none()
             }
-            Message::NorthWest => {
-                window::get_latest().and_then(|f| drag_resize(f, window::Direction::NorthWest))
-            }
-            Message::North => {
-                window::get_latest().and_then(|f| drag_resize(f, window::Direction::North))
-            }
-            Message::NorthEast => {
-                window::get_latest().and_then(|f| drag_resize(f, window::Direction::NorthEast))
-            }
-            Message::West => {
-                window::get_latest().and_then(|f| drag_resize(f, window::Direction::West))
-            }
-            Message::East => {
-                window::get_latest().and_then(|f| drag_resize(f, window::Direction::East))
-            }
-            Message::South => {
-                window::get_latest().and_then(|f| drag_resize(f, window::Direction::South))
-            }
-            Message::SouthWest => {
-                window::get_latest().and_then(|f| drag_resize(f, window::Direction::SouthWest))
-            }
-            Message::SouthEast => {
-                window::get_latest().and_then(|f| drag_resize(f, window::Direction::SouthEast))
-            }
-            Message::Close => window::get_latest().and_then(window::close),
         }
     }
-
     fn view(&self) -> Element<Message> {
-        let bottom_row = row![
-            mouse_area(
-                container(row![])
-                    .width(5)
-                    .height(2)
-                    .style(|_| border_container())
-            )
-            .on_press(Message::SouthWest)
-            .interaction(Interaction::ResizingDiagonallyUp),
-            mouse_area(
-                container(row![])
-                    .width(Length::Fill)
-                    .height(2)
-                    .style(|_| border_container())
-            )
-            .on_press(Message::South)
-            .interaction(Interaction::ResizingVertically),
-            mouse_area(
-                container(row![])
-                    .width(5)
-                    .height(2)
-                    .style(|_| border_container())
-            )
-            .on_press(Message::SouthEast)
-            .interaction(Interaction::ResizingDiagonallyDown),
-        ];
-
-        let top_row = row![
-            mouse_area(
-                container(row![])
-                    .width(5)
-                    .height(2)
-                    .style(|_| border_container())
-            )
-            .on_press(Message::NorthWest)
-            .interaction(Interaction::ResizingDiagonallyDown),
-            mouse_area(
-                container(row![])
-                    .width(Length::Fill)
-                    .height(2)
-                    .style(|_| border_container())
-            )
-            .on_press(Message::North)
-            .interaction(Interaction::ResizingVertically),
-            mouse_area(
-                container(row![])
-                    .width(5)
-                    .height(2)
-                    .style(|_| border_container())
-            )
-            .on_press(Message::NorthEast)
-            .interaction(Interaction::ResizingDiagonallyUp),
-        ];
-
-        container(column![
-            top_row,
-            container(
-                row![
-                    mouse_area(
-                        container(row![])
-                            .width(2)
-                            .height(Fill)
-                            .style(|_| border_container())
-                    )
-                    .on_press(Message::West)
-                    .interaction(Interaction::ResizingHorizontally),
-                    container(column![
-                        mouse_area(
-                            container(
-                                row![
-                                    button(text("x").center())
-                                        .padding(0)
-                                        .width(20)
-                                        .on_press(Message::Close)
-                                ]
-                                .padding(4)
-                            )
-                            .style(|t: &Theme| container::Style {
-                                background: Some(Background::Color(t.palette().success)),
-                                border: iced::Border {
-                                    color: t.palette().warning,
-                                    width: 2.0,
-                                    radius: 8.into()
-                                },
-                                ..Default::default()
-                            })
-                            .width(Length::Fill)
-                            .height(30)
-                        )
-                        .on_press(Message::Drag),
-                    ])
-                    // .padding(10)
-                    .style(|t: &Theme| Style {
-                        background: Some(Background::Color(t.palette().primary)),
-                        border: iced::Border {
-                            color: t.palette().danger,
-                            width: 2.,
-                            radius: 8.into(),
-                        },
-                        ..Default::default()
-                    })
-                    .width(Fill)
-                    .height(Fill),
-                    mouse_area(
-                        container(row![])
-                            .width(2)
-                            .height(Fill)
-                            .style(|_| border_container())
-                    )
-                    .on_press(Message::East)
-                    .interaction(Interaction::ResizingHorizontally),
-                ]
-                .width(Fill)
-                .height(Fill)
-            )
-            .width(Fill)
-            .height(Fill),
-            bottom_row
-        ])
-        .width(Fill)
-        .height(Fill)
-        .into()
-    }
-}
-
-fn border_container() -> Style {
-    container::Style {
-        background: Some(Background::Color(Color::TRANSPARENT)),
-        ..Default::default()
+        utils::dragwin::view().map(|_f| Message::Shakira)
     }
 }
---
+++ src/utils/dragwin.rs
@@ -0,0 +1,191 @@
+use iced::{
+    Background, Color, Element,
+    Length::Fill,
+    Task, Theme,
+    mouse::Interaction,
+    widget::{
+        button,
+        container::{self, Style},
+        mouse_area, row, text,
+    },
+    window::{self, drag_resize},
+};
+
+use crate::Example;
+
+#[derive(Debug, Clone)]
+pub enum Message {
+    Drag,
+    Maximize,
+    NorthWest,
+    North,
+    NorthEast,
+    West,
+    East,
+    South,
+    SouthWest,
+    SouthEast,
+    Close,
+}
+
+pub fn update(message: Message) -> Task<Message> {
+    match message {
+        Message::Drag => {
+            window::get_latest().and_then(window::drag)
+            // Task::none()
+        }
+        Message::Maximize => {
+            println!("asd");
+            // Task::none()
+            window::get_latest().and_then(|t| window::maximize(t, true))
+        }
+        Message::NorthWest => {
+            window::get_latest().and_then(|f| drag_resize(f, window::Direction::NorthWest))
+        }
+        Message::North => {
+            window::get_latest().and_then(|f| drag_resize(f, window::Direction::North))
+        }
+        Message::NorthEast => {
+            window::get_latest().and_then(|f| drag_resize(f, window::Direction::NorthEast))
+        }
+        Message::West => window::get_latest().and_then(|f| drag_resize(f, window::Direction::West)),
+        Message::East => window::get_latest().and_then(|f| drag_resize(f, window::Direction::East)),
+        Message::South => {
+            window::get_latest().and_then(|f| drag_resize(f, window::Direction::South))
+        }
+        Message::SouthWest => {
+            window::get_latest().and_then(|f| drag_resize(f, window::Direction::SouthWest))
+        }
+        Message::SouthEast => {
+            window::get_latest().and_then(|f| drag_resize(f, window::Direction::SouthEast))
+        }
+        Message::Close => window::get_latest().and_then(window::close),
+    }
+}
+
+pub fn view() -> Element<'static, Message> {
+    let base = iced::widget::container(iced::widget::column![
+        mouse_area(
+            iced::widget::container(
+                row![button(text("x")).on_press(Message::Close).padding(0)].padding(4)
+            )
+            // .style(|t: &Theme| container::Style {
+            //     background: Some(Background::Color(t.palette().success)),
+            //     border: iced::Border {
+            //         color: t.palette().warning,
+            //         width: 2.0,
+            //         radius: 8.into()
+            //     },
+            //     ..Default::default()
+            // })
+            .width(Fill)
+            .height(30)
+        )
+        .on_right_press(Message::Maximize)
+        .on_press(Message::Drag),
+    ])
+    .style(|t: &Theme| Style {
+        background: Some(Background::Color(t.palette().background)),
+        border: iced::Border {
+            color: t.palette().warning,
+            width: 1.,
+            radius: 8.into(),
+        },
+        ..Default::default()
+    })
+    .width(Fill)
+    .height(Fill);
+    let bottom_row = row![
+        mouse_area(
+            iced::widget::container(row![])
+                .width(5)
+                .height(2)
+                .style(|_| border_container())
+        )
+        .on_press(Message::SouthWest)
+        .interaction(Interaction::ResizingDiagonallyUp),
+        mouse_area(
+            iced::widget::container(row![])
+                .width(Fill)
+                .height(2)
+                .style(|_| border_container())
+        )
+        .on_press(Message::South)
+        .interaction(Interaction::ResizingVertically),
+        mouse_area(
+            iced::widget::container(row![])
+                .width(5)
+                .height(2)
+                .style(|_| border_container())
+        )
+        .on_press(Message::SouthEast)
+        .interaction(Interaction::ResizingDiagonallyDown),
+    ];
+
+    let top_row = row![
+        mouse_area(
+            iced::widget::container(row![])
+                .width(5)
+                .height(2)
+                .style(|_| border_container())
+        )
+        .on_press(Message::NorthWest)
+        .interaction(Interaction::ResizingDiagonallyDown),
+        mouse_area(
+            iced::widget::container(row![])
+                .width(Fill)
+                .height(2)
+                .style(|_| border_container())
+        )
+        .on_press(Message::North)
+        .interaction(Interaction::ResizingVertically),
+        mouse_area(
+            iced::widget::container(row![])
+                .width(5)
+                .height(2)
+                .style(|_| border_container())
+        )
+        .on_press(Message::NorthEast)
+        .interaction(Interaction::ResizingDiagonallyUp),
+    ];
+
+    let rebase: Element<_> = iced::widget::container(iced::widget::column![
+        top_row,
+        iced::widget::container(
+            row![
+                mouse_area(
+                    iced::widget::container(row![])
+                        .width(2)
+                        .height(Fill)
+                        .style(|_| border_container())
+                )
+                .on_press(Message::West)
+                .interaction(Interaction::ResizingHorizontally),
+                base,
+                mouse_area(
+                    iced::widget::container(row![])
+                        .width(2)
+                        .height(Fill)
+                        .style(|_| border_container())
+                )
+                .on_press(Message::East)
+                .interaction(Interaction::ResizingHorizontally),
+            ]
+            .width(Fill)
+            .height(Fill)
+        )
+        .width(Fill)
+        .height(Fill),
+        bottom_row
+    ])
+    .width(Fill)
+    .height(Fill)
+    .into();
+    rebase
+}
+fn border_container() -> Style {
+    container::Style {
+        background: Some(Background::Color(Color::TRANSPARENT)),
+        ..Default::default()
+    }
+}
